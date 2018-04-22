a8fba1b4ecd14d168fb42bbfe4be8f6359fd9119
aa153544 <anzaiyu@jp.ibm.com>
2018-04-22 18:20:16 +0900

    control-syntax-if

---
+++ src/main/scala/Study_if.scala
@@ -0,0 +1,5 @@
+if (age < 18) {
+  println "18歳未満です"
+} else {
+  println "18歳以上です"
+}
---
+++ src/main/scala/Study_if_test.scala
@@ -0,0 +1,10 @@
+var age = 7
+var isSchoolStarted: Boolean = false
+
+if (1 <= age && age <= 6 ) isSchoolStarted = true
+
+if (isSchoolStarted == true) {
+  println("幼児です")
+} else {
+  println("幼児ではありません")
+}
